59d440e9e136bdd1c6b786316b59cc0964d3f99c
Ik ku dar anai <shvier@shvierdemacbook-pro.local>
2016-01-13 17:47:00 +0800

    starting my login system

---
+++ LoginSystem/LoginSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginSystem
+//
+//  Created by Shvier on 16/1/13.
+//  Copyright © 2016年 Shvier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (retain, nonatomic) UIWindow *window;
+
+
+@end
+
